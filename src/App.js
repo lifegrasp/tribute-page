677b69e16e940ee05910c914b33410971af0f6b4
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2022-11-12 14:34:51 +0800

    configured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="underline">Header One</h1>
+      <h2>Title</h2>
+      <p>Paragraph demo</p>
     </div>
   );
 }
